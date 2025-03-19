fda5efc580dc8209d4dd573fb6fb3aa54d5f35cf
lizazun <136836204+lizazun@users.noreply.github.com>
2025-03-19 11:55:00 +0900

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,26 @@
+def get_integer(prompt):
+    return int(input(prompt))
+
+def exchange(amount):
+  print("동전으로 교환하고자 하는 금액은?", amount )
+
+  n500 = amount// 500
+  amount%= 500
+
+  n100 = amount // 100
+  amount%= 100
+
+  n50 = amount // 50
+  amount%= 50
+
+  n10 = amount // 10
+  amount%= 10
+
+  print('>500원 동전의 개수:', n500, )
+  print('>100원 동전의 개수:', n100,)
+  print('>50원 동전의 개수:', n50, )
+  print('>10원 동전의 개수:', n10, )
+
+amount = get_integer("동전으로 교환하고자 하는 금액은? ")
+
+exchange(amount)
