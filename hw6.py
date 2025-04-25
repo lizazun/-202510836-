d96bd97d5b8a28e20d17a95c787fc6e68fe96ecd
lizazun <136836204+lizazun@users.noreply.github.com>
2025-04-25 15:50:58 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,10 @@
+def print_gugudan(start, end):
+    for i in range(1, 10): 
+        for dan in range(start, end + 1):  
+            print(f"{dan}x{i}={dan*i:2}", end="\t") 
+        print() 
+
+print_gugudan(2, 5)
+print()  
+
+print_gugudan(6, 9)
