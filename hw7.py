b5a5d6b1ae5004f4f7335c274027eb50b456b7da
lizazun <136836204+lizazun@users.noreply.github.com>
2025-04-29 17:40:40 +0900

    Add files via upload

---
+++ hw7.py
@@ -0,0 +1,20 @@
+shopping_bag = {}
+
+while True:
+    item = input("상품명? ")
+    if item == "":
+        break
+    else :
+        amount = int(input("갯수는? "))
+        shopping_bag[item] = amount  
+        print(f"장바구니에 {item} {amount}개가 담겼습니다.")
+
+print("\n>>> 장바구니 보기:", shopping_bag)
+
+print("\n[검색]")
+search_item = input("장바구니에서 확인하고자 하는 상품은? ")
+
+if search_item in shopping_bag:
+    print(f"{search_item}은(는) {shopping_bag[search_item]}개 담겨 있습니다.")
+else:
+    print(f"{search_item}은(는) 장바구니에 없습니다.")
